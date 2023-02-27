7b7639ae5c127f1f01f2c0b5afe053e73a53202e
RichardMakkonen18 <hackmakkonen@gmail.com>
2023-02-27 15:33:08 -0600

    commit bd in component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,33 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private db:AngularFireDatabase) {
-    //MODIFICAR VALORES DEVARIABLE CON SU VALOR 
-    this.db.object("alerta").set(true);
-    this.db.object("contactos").set(true);
+  constructor(private db: AngularFireDatabase) {
+    //MODIFICAR VALORES DEVARIABLE CON SU VALOR
+    this.db.object('user').set(true);
+    this.db.object('contactos').set(true);
 
-   
-    this.db.list("contactos").push({nombre: "EJEMPLO", apellido:"EJE", edad:19, numero:"7272727"})
-
-    //DETECTAR CAMBIOS DE VARIABLE 
-          this.db.object("alerta").valueChanges().subscribe(data=>{
-          console.log(data);  
-    })
+    //DETECTAR CAMBIOS DE VARIABLE
+    this.db
+      .object('alerta')
+      .valueChanges()
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
-          
-  registrarContacto(){
-    this.db.list("contactos").push({nombre: "EJEMPLO", apellido:"EJE", edad:19, numero:"7272727"})
+
+  registrarContacto() {
+    this.db.list('contactos').push({ nombre: 'kdddk', telefono: '3463637383' });
+  }
+
+  registrarUser() {
+    this.db
+      .list('user')
+      .push({
+        nombre: ' ',
+        apellido: ' ',
+        edad: 19,
+        telefono: '4493011624',
+        email: 'example@example.com',
+      });
   }
 }
-
